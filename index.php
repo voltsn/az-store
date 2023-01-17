a5c218a8964a10ce1e20850f2612e2bd03f88125
MathildeCornelis <mathilde.cornelis26@gmail.com>
2023-01-17 14:19:36 +0100

    scss marketing

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
-<?php require "./src/inc/header.php"; ?>
-    <section>
-        <div>
-            <h1>Shoe the right <span>one</span>.</h1>
-            <button>See our store</button>
-        </div>
-        <div>
-            <img src="" alt="Nike svg">
-            <img src="src\img\shoe_one.png" alt="shoe Nike">
-        </div>
-    </section>
-    <section>
-        <h3<span>Our</span> last products</h2>
+<?php //require "./src/inc/header.php"; 
+?>
+<!--
+<section>
+    <div>
+        <h1>Shoe the right <span>one</span>.</h1>
+        <button>See our store</button>
+    </div>
+    <div>
+        <img src="" alt="Nike svg">
+        <img src="src\img\shoe_one.png" alt="shoe Nike">
+    </div>
+</section>
+<section>
+    <h3<span>Our</span> last products</h2>
         <div>
             <div>
                 <img src="src\img\shoe_one.png" alt="shoe Nike">
@@ -19,12 +21,14 @@
                 <button>Add to card</button>
             </div>
         </div>
-    </section>
-    <section>
-        <img src="src\img\shoe_two.png" alt="shoe Nike">
-        <h2>We provide you the <span>best</span>quality</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, dolorem dolores? Maxime molestias possimus nobis amet odio sequi incidunt nostrum porro! Totam dolorum dignissimos placeat culpa repellendus distinctio deserunt cum!</p>
-    </section>
+</section>
+-->
+<section class="marketing">
+    <img class="shoe" src="src\img\shoe_two.png" alt="shoe Nike">
+    <h2>We provide you the <span>best</span>quality</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, dolorem dolores? Maxime molestias possimus nobis amet odio sequi incidunt nostrum porro! Totam dolorum dignissimos placeat culpa repellendus distinctio deserunt cum!</p>
+</section>
+<!--
     <div>
         <article>
             <img src="src\img\image-emily.jpg" alt="Picture of Emily">
@@ -42,4 +46,6 @@
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus corporis hic esse et, odio quos eum officia velit, illum ipsam, sed maxime. Eveniet, eius recusandae dolor non quidem animi qui.</p>
         </article>
     </div>
-<?php require "./src/inc/footer.php"; ?>
+    -->
+<?php //require "./src/inc/footer.php"; 
+?>
--- src/inc/header.php
+++ src/inc/header.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AZ store</title>
+    <link rel="stylesheet" href="main.css">
 </head>
+
 <body>
     <header>
         <img src="src\img\AZstore_1.png" alt="Logo white AZ store">
@@ -20,6 +23,6 @@
                     <a href="#">Login</a>
                 </li>
             </ul>
-        </nav> 
+        </nav>
     </header>
     <main>
