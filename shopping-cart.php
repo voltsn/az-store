94115308e7464f87974858e9b4f94f9dd24f7914
Milo Bonnet <milovtt@gmail.com>
2023-01-17 16:41:31 +0100

    Finish shopping-card

--- shopping-cart.php
+++ shopping-cart.php
@@ -1,3 +1,31 @@
 <?php  require "./src/inc/header.php"; ?>
-// Contenu ici
+<section class="card-main">
+    <h2 class="card-title">Your <span>cart</span> (X Articles)</h2>
+    <div class="basket-main">
+        <img src="./public/images/products/shoe_two.png" alt="Chaussure AirMax" class="card-img">
+        <div class="info-card">
+            <p class="card-name">NIKE AIR</p>
+            <select name="" id=""></select>
+            <select name="" id=""></select>
+            <div class="total-card">
+                <p class="card-price">234€</p>
+                <img src="./public/images/icons/delete 1.svg" alt="" class="card-delete">
+            </div>
+        </div>
+    </div>
+    <div class="basket-main">
+        <img src="./public/images/products/shoe_two.png" alt="Chaussure AirMax" class="card-img">
+        <div class="info-card">
+            <p class="card-name">NIKE AIR</p>
+            <select name="" id=""></select>
+            <select name="" id=""></select>
+            <div class="total-card">
+                <p class="card-price">234€</p>
+                <img src="./public/images/icons/delete 1.svg" alt="" class="card-delete">
+            </div>
+        </div>
+    </div>
+    <p class="total-price">Total: 468€</p>
+    <button class="card-btn">Go to checkout</button>
+</section>
 <?php require "./src/inc/footer.php"; ?>
