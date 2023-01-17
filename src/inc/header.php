596f454b34ac926e783953006596844e4e4d4eac
MathildeCornelis <mathilde.cornelis26@gmail.com>
2023-01-17 16:25:12 +0100

    favicon

--- src/inc/header.php
+++ src/inc/header.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./public/css/main.css">
     <title>AZ store</title>
+    <link rel="shortcut icon" href="src\img\favicon.png" type="image/x-icon">
 </head>
 <!--
 <body>
