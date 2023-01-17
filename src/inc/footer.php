584167b60c20e89a0ca79e4b6e2f169e1088a9b1
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-01-17 12:37:10 +0100

    Create inc folder

---
+++ src/inc/footer.php
@@ -0,0 +1,12 @@
+</main>
+    <footer>
+        <p>Dev in becode with ❤️</p>
+        <ul>
+            <li><a href="#">Arno</a></li>
+            <li><a href="#">Dominique</a></li>
+            <li><a href="#">Mathilde</a></li>
+            <li><a href="#">Marnie</a></li>
+            <li><a href="#">Milo</a></li>
+    </footer>
+</body>
+</html>
---
+++ src/inc/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AZ store</title>
+</head>
+<body>
+    <header>
+        <img src="src\img\AZstore_1.png" alt="Logo white AZ store">
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Products</a></li>
+                <li><a href="#">Contact</a></li>
+                <li>
+                    <a href="#"><img src="src\img\shopping-cart.svg" alt="Shopping cart"></a>
+                    <a href="#">Login</a>
+                </li>
+            </ul>
+        </nav> 
+    </header>
+    <main>
