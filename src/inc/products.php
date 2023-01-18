aa40a17801b3d97452b341ae064c74f8df325fb7
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-01-18 10:14:23 +0100

    Add id's to products

--- src/inc/products.php
+++ src/inc/products.php
@@ -1,21 +1,25 @@
 <?php 
     $products = array(
         "Nike Air" => array(
+            "id" => 1,
             "img" => "src\img\shoe_one.png",
             "alt" => "Nike shoe",
             "price" => 234
             ),
         "Nike Air" => array(
+            "id" => 2,
             "img" => "src\img\shoe_one.png",
             "alt" => "Nike shoe",
             "price" => 234
             ),
         "Nike Air" => array(
+            "id" => 3,
             "img" => "src\img\shoe_one.png",
             "alt" => "Nike shoe",
             "price" => 234
             ),
         "Nike Air" => array(
+            "id" => 4,
             "img" => "src\img\shoe_one.png",
             "alt" => "Nike shoe",
             "price" => 234
