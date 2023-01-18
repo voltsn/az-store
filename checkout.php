5045eac052ef0af28bc9479799cf8ac9e81f4391
MathildeCornelis <mathilde.cornelis26@gmail.com>
2023-01-18 14:22:28 +0100

    ;

--- checkout.php
+++ checkout.php
@@ -1,47 +1,45 @@
-<?php 
-    // Todo: Include the country array and display them in the form
-    // Todo: Using $_SESSION retrieve the products from the cart and
-    //       display the contents of the cart in checkout-cart div
-    // Todo: If the form has been submitted (through POST) validate and sanitaze input
-    // Todo: If the data from the form is valid empty the cart and display a succes msg
-    //       instead of the form, if one of the inputs was invalid add a red outline to the input
-    // Note: php tags can be inserted inbetween HTML tags like so: <h1> <?php $name ? > </h1>
-    // Note: HTML can be generated in php simply using echo:
-    //        <?php echo "<div class='some-class'> $SomeVariable </div>"? >
+<?php
+// Todo: Include the country array and display them in the form
+// Todo: Using $_SESSION retrieve the products from the cart and
+//       display the contents of the cart in checkout-cart div
+// Todo: If the form has been submitted (through POST) validate and sanitaze input
+// Todo: If the data from the form is valid empty the cart and display a succes msg
+//       instead of the form, if one of the inputs was invalid add a red outline to the input
+// Note: php tags can be inserted inbetween HTML tags like so: <h1> <?php $name ? > </h1>
+// Note: HTML can be generated in php simply using echo:
+//        <?php echo "<div class='some-class'> $SomeVariable </div>"? >
 ?>
-<?php  require "./src/inc/header.php"; ?>
+<?php require "./src/inc/header.php"; ?>
 <article class="form-container">
     <div class="form-header">
         <h1 class="form-header-heading">
             Checkout
         </h1>
 
-            <?php 
-            // Using $_SESSION retrieve the products from the cart and display the contents of the cart in checkout-cart div
-            // Storing the product when added to the cart in a session variable (array).
-            session_start(); // Starting the session before accessing the session variable
-            $produits = NULL;
-            if (!isset($_SESSION['cart'])) {
-            
-            }
-            else {
-                $produits = $_SESSION["cart"];
-            }
-            ?>
-        <div class="checkout-cart">
         <?php
-        // Display the content of the cart here, by using a loop to browse the session array and generate the contents of the div.
-        if ($produits) {
-            foreach ($produits as $item) {
-                echo "<p> $item[name] </p>
-                <p> $item[size] </p>
-                <p> $item[quantity] </p>
-                <p> $item[price] </p>"
-            }
+        // Using $_SESSION retrieve the products from the cart and display the contents of the cart in checkout-cart div
+        // Storing the product when added to the cart in a session variable (array).
+        session_start(); // Starting the session before accessing the session variable
+        $produits = NULL;
+        if (!isset($_SESSION['cart'])) {
         } else {
-            echo '<p>Your cart is empty.</p>';
+            $produits = $_SESSION["cart"];
         }
         ?>
+        <div class="checkout-cart">
+            <?php
+            // Display the content of the cart here, by using a loop to browse the session array and generate the contents of the div.
+            if ($produits) {
+                foreach ($produits as $item) {
+                    echo "<p> $item[name] </p>
+                <p> $item[size] </p>
+                <p> $item[quantity] </p>
+                <p> $item[price] </p>";
+                }
+            } else {
+                echo '<p>Your cart is empty.</p>';
+            }
+            ?>
         </div>
     </div>
 
@@ -63,7 +61,7 @@
             </div>
 
             <div class="form-control">
-                <input class="form-input" id="address" type="text"  name="address" required>
+                <input class="form-input" id="address" type="text" name="address" required>
                 <label for="address"> Address </label>
             </div>
 
@@ -80,44 +78,44 @@
             <div class="form-control">
                 <label for="country">Country : </label>
                 <select class="form-input" name="country" id="country" required>
-                    <?php 
-                        // Generate the options here
-                        // Country array
-                        $countries = array(
-                            "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda",
-                            "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain",
-                            "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia",
-                            "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso",
-                            "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Central African Republic",
-                            "Chad", "Chile", "China", "Colombia", "Comoros", "Congo", "Costa Rica", "Croatia",
-                            "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic",
-                            "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea",
-                            "Estonia", "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia",
-                            "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau",
-                            "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran",
-                            "Iraq", "Ireland", "Israel", "Italy", "Ivory Coast", "Jamaica", "Japan", "Jordan",
-                            "Kazakhstan", "Kenya", "Kiribati", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia",
-                            "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg",
-                            "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta",
-                            "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova",
-                            "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar", "Namibia",
-                            "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Korea",
-                            "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru",
-                            "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis",
-                            "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia",
-                            "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Korea", "South Sudan",
-                            "Spain", "Sri Lanka", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria",
-                            "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Togo", "Tonga", "Trinidad and Tobago",
-                            "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates",
-                            "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City",
-                            "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe"
-                        );
-                        // Display the countries in the form                        
-                        foreach ($countries as $country) {
-                        echo '<option value="'.$country.'">'.$country.'</option>';
-                        }
+                    <?php
+                    // Generate the options here
+                    // Country array
+                    $countries = array(
+                        "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda",
+                        "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain",
+                        "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia",
+                        "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso",
+                        "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Central African Republic",
+                        "Chad", "Chile", "China", "Colombia", "Comoros", "Congo", "Costa Rica", "Croatia",
+                        "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic",
+                        "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea",
+                        "Estonia", "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia",
+                        "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau",
+                        "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran",
+                        "Iraq", "Ireland", "Israel", "Italy", "Ivory Coast", "Jamaica", "Japan", "Jordan",
+                        "Kazakhstan", "Kenya", "Kiribati", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia",
+                        "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg",
+                        "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta",
+                        "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova",
+                        "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar", "Namibia",
+                        "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Korea",
+                        "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru",
+                        "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis",
+                        "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia",
+                        "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Korea", "South Sudan",
+                        "Spain", "Sri Lanka", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria",
+                        "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Togo", "Tonga", "Trinidad and Tobago",
+                        "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates",
+                        "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City",
+                        "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe"
+                    );
+                    // Display the countries in the form                        
+                    foreach ($countries as $country) {
+                        echo '<option value="' . $country . '">' . $country . '</option>';
+                    }
                     ?>
-</select>
+                </select>
                 </select>
             </div>
 
