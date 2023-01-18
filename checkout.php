d9e4186f061e67df18a1fd3143cb35bb68db4742
DCoppee <dominiquecoppee.pro@gmail.com>
2023-01-18 10:36:27 +0100

    form

--- checkout.php
+++ checkout.php
@@ -16,8 +16,31 @@
             Checkout
         </h1>
 
+            <?php 
+            // Using $_SESSION retrieve the products from the cart and display the contents of the cart in checkout-cart div
+            // Storing the product when added to the cart in a session variable (array).
+            session_start(); // Starting the session before accessing the session variable
+            if (!isset($_SESSION['cart'])) {
+            $_SESSION['cart'] = array();
+            }
+            $_SESSION['cart'][] = array(
+                'shoes_name' => $shoes_name,
+                'shoes_id' => $shoes_id,
+                'shoes_price' => $shoes_price,
+                'quantity' => $quantity
+            );
+            ?>
+            // Display the content of the cart here, by using a loop to browse the session array and generate the contents of the div.
         <div class="checkout-cart">
-            <?php // The content of the cart goes here ?>
+        <?php
+        if (isset($_SESSION['cart']) && !empty($_SESSION['cart'])) {
+            foreach ($_SESSION['cart'] as $item) {
+                // ici, je cale
+            }
+        } else {
+            echo '<p>Your cart is empty.</p>';
+        }
+        ?>
         </div>
     </div>
 
@@ -58,7 +81,42 @@
                 <select class="form-input" name="country" id="country" required>
                     <?php 
                         // Generate the options here
+                        // Country array
+                        $countries = array(
+                            "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda",
+                            "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain",
+                            "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia",
+                            "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso",
+                            "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Central African Republic",
+                            "Chad", "Chile", "China", "Colombia", "Comoros", "Congo", "Costa Rica", "Croatia",
+                            "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic",
+                            "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea",
+                            "Estonia", "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia",
+                            "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau",
+                            "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran",
+                            "Iraq", "Ireland", "Israel", "Italy", "Ivory Coast", "Jamaica", "Japan", "Jordan",
+                            "Kazakhstan", "Kenya", "Kiribati", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia",
+                            "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg",
+                            "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta",
+                            "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova",
+                            "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar", "Namibia",
+                            "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Korea",
+                            "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru",
+                            "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis",
+                            "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia",
+                            "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Korea", "South Sudan",
+                            "Spain", "Sri Lanka", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria",
+                            "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Togo", "Tonga", "Trinidad and Tobago",
+                            "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates",
+                            "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City",
+                            "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe"
+                        );
+                        // Display the countries in the form                        
+                        foreach ($countries as $country) {
+                        echo '<option value="'.$country.'">'.$country.'</option>';
+                        }
                     ?>
+</select>
                 </select>
             </div>
 
