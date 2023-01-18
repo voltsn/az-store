61acac2d7c68b8252fc3d8bae2255e86c98780b8
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-01-18 15:51:55 +0100

    Improve hero section layout

--- src/inc/products.php
+++ src/inc/products.php
@@ -1,28 +1,40 @@
 <?php 
-    $products = array(
-        "Nike Air" => array(
+    $products = [
+
+        [            
             "id" => 1,
+            "name" => "NIKE Air",
             "img" => "src\img\shoe_one.png",
             "alt" => "Nike shoe",
             "price" => 234
-            ),
-        "Nike Air" => array(
+        ],
+        [            
             "id" => 2,
+            "name" => "NIKE Air",
             "img" => "src\img\shoe_one.png",
             "alt" => "Nike shoe",
             "price" => 234
-            ),
-        "Nike Air" => array(
+        ],
+        [            
             "id" => 3,
+            "name" => "NIKE Air",
             "img" => "src\img\shoe_one.png",
             "alt" => "Nike shoe",
             "price" => 234
-            ),
-        "Nike Air" => array(
+        ],
+        [            
             "id" => 4,
+            "name" => "NIKE Air",
             "img" => "src\img\shoe_one.png",
             "alt" => "Nike shoe",
             "price" => 234
-            ),
-        );
+        ],
+        [            
+            "id" => 4,
+            "name" => "NIKE Air",
+            "img" => "src\img\shoe_one.png",
+            "alt" => "Nike shoe",
+            "price" => 234
+        ]
+    ]
 ?>
